eab0d3a8229bf0e2d0baf8431638985b3e11f587
xu3545k7 <xu3545k7@gmail.com>
2025-09-03 15:28:16 +0800

    deployed 03

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
 
-module.exports = {
+module.exports = defineConfig({
+  transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/Menu-project/'
     : '/'
-}
+})
